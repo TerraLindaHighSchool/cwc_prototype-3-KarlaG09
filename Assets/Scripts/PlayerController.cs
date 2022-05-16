87a22e1aa89492abc9fb7d2474debf75ac18bdab
KarlaG09 <kg02793@srcs.org>
2022-05-16 15:49:56 -0700

    Unit 3 - Sound and Effects, Lesson 3.3 - Don't Just Stand There, Tutorial 6.Keep player from unconscious jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
-    public float jumpForce = 10;
+    private Animator playerAnim; 
+    public float jumpForce = 150;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
@@ -13,16 +14,18 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>(); 
         Physics.gravity *= gravityModifier; 
     }
 
     // Update is called once per frame
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+      if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig"); 
         }
     }
 
@@ -34,7 +37,9 @@ public class PlayerController : MonoBehaviour
         } else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over");
-            gameOver =true; 
+            gameOver =true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int ", 1); 
         }
     } 
 }
